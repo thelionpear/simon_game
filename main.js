c8a2a6c2104d90a57cbe701dc10d43d5dc042db8
Michelle Garcia <thelionpear@gmail.com>
2018-05-17 14:27:29 -0600

    light up function and turn off function complete

--- main.js
+++ main.js
@@ -1,22 +1,46 @@
-//need a function that lights up the margins of the buttons 
-$(".game_button").light_up(function(button_name) {
-    $(".button_name").removeClass("opacity");
-    $(".button_name").addClass("not_opaque"); 
-});
-//need a function that generates the computer's color array 
 
-//need a function that lights up the margins based on the computer array
+// $(".game_button").light_up(function(button_name) {
+//     $(".button_name").removeClass("opacity");
+//     $(".button_name").addClass("not_opaque"); 
+// });
+
+//need a function that lights up the buttons 
+function lightUp(button_name) {
+    $(button_name).removeClass("opacity");
+    $(button_name).addClass("not_opaque"); 
+}
+//function that turns off the buttons 
+function lightOff(button_name) {
+    $(button_name).removeClass("not_opaque");
+    $(button_name).addClass("opacity"); 
+}
+//generates the computer's color array 
+
+//lights up buttons based on the computer array
 
 //adds one to the computer array each time 
 
-//need a function that takes what the user does into an array 
+//takes what the user does into an array 
 
-//need a function that let's the user submit their choices 
+//let's the user submit their choices 
 
-//need a function that compares what the computer did and what the user did 
+//compares what the computer did and what the user did 
 
-//need a function that takes the comparison and displays if you got it or not 
+//takes the comparison and displays if you got it or not 
 
-//need a function that tracks a user and their high score 
+//tracks a user and their high score 
 
-light_up("red_button")
+// var colors = ['.red_button', '.blue_button', '.yellow_button', '.green_button']
+// colors.forEach( function(color) {
+//     setTimeout( function() {
+//       suspense(color);
+//     }, 1000)
+// })
+
+// function suspense(color) {
+//     setTimeout( function() {
+//       lightUp(color)
+//     }, 1000)
+// }
+
+lightUp(".red_button")
