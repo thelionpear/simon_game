c5f88a07471faefc22c6761c642f585ef0f2580f
Michelle Garcia <thelionpear@gmail.com>
2018-05-17 13:48:06 -0600

    layout is complete, functions not started

---
+++ main.js
@@ -0,0 +1,22 @@
+//need a function that lights up the margins of the buttons 
+$(".game_button").light_up(function(button_name) {
+    $(".button_name").removeClass("opacity");
+    $(".button_name").addClass("not_opaque"); 
+});
+//need a function that generates the computer's color array 
+
+//need a function that lights up the margins based on the computer array
+
+//adds one to the computer array each time 
+
+//need a function that takes what the user does into an array 
+
+//need a function that let's the user submit their choices 
+
+//need a function that compares what the computer did and what the user did 
+
+//need a function that takes the comparison and displays if you got it or not 
+
+//need a function that tracks a user and their high score 
+
+light_up("red_button")
