9ff2f848c2c750a6a6448f35a250364f5b874ba7
Michelle Garcia <thelionpear@gmail.com>
2018-05-17 21:30:56 -0600

    establish computer array function and play game function that lights up buttons based on computer array

--- main.js
+++ main.js
@@ -15,13 +15,47 @@ function lightOff(button_name) {
     $(button_name).addClass("opacity"); 
 }
 //generates the computer's color array 
+var array_colors = ["red", "blue", "yellow", "green"]
+var computer_array = []
+
+
+function computer_array() {
+    var random_number = Math.floor(Math.random() * array_colors.count)
+    var computer_choice = array_colors[random_number] 
+    computer_array.push(computer_choice) 
+}
 
 //lights up buttons based on the computer array
 
+
+function play_game() {
+    foreach(color, computer_array)
+    switch(color) {
+        case "red": 
+        lightUp("red_button")
+        case "blue": 
+        lightUp("blue_button")
+        case "yellow":
+        lightUp("yellow_button")
+        case "green": 
+        lightUp("green_button")
+    } 
+}
 //adds one to the computer array each time 
 
 //takes what the user does into an array 
+var user_array = []
 
+function user_choice() {
+    $("#.red_button").onClick(function {
+        var choice = "red" 
+    })
+}
+
+function user_array_choice() {
+    switch(choice)
+    case ()
+}
 //let's the user submit their choices 
 
 //compares what the computer did and what the user did 
