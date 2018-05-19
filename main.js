236d4af7c22a773b3a5272a430131e1c56b318ad
Michelle Garcia <thelionpear@gmail.com>
2018-05-18 20:14:58 -0600

    game works but computer starts its job too soon after user input

--- main.js
+++ main.js
@@ -4,61 +4,138 @@
 //     $(".button_name").addClass("not_opaque"); 
 // });
 
+
+$(document).ready( function() {
+
+    var arrayColors = ["red", "blue", "yellow", "green"];
+    var computerArray = [];
+    addEventsToButtons();
+    addToComputerArray();
 //need a function that lights up the buttons 
-function lightUp(button_name) {
-    $(button_name).removeClass("opacity");
-    $(button_name).addClass("not_opaque"); 
-}
-//function that turns off the buttons 
-function lightOff(button_name) {
-    $(button_name).removeClass("not_opaque");
-    $(button_name).addClass("opacity"); 
-}
-//generates the computer's color array 
-var array_colors = ["red", "blue", "yellow", "green"]
-var computer_array = []
+    
+    //function that turns off the buttons 
+   
+    //generates the computer's color array 
+    
+   
 
 
-function computer_array() {
-    var random_number = Math.floor(Math.random() * array_colors.count)
-    var computer_choice = array_colors[random_number] 
-    computer_array.push(computer_choice) 
-}
+    function addToComputerArray() {
+        var randomColor = arrayColors[Math.floor(Math.random() * arrayColors.length)];
+        computerArray.push(randomColor); 
+        playGame(); 
+    }
 
-//lights up buttons based on the computer array
+    //lights up buttons based on the computer array
 
 
-function play_game() {
-    foreach(color, computer_array)
-    switch(color) {
+    function playGame() {
+        var time = 0;
+        computerArray.forEach(function (color) {
+            setTimeout(function() { 
+                switch(color) {
+                    case "red": 
+                    lightUp("red_button");
+                    break;
+                    case "blue": 
+                    lightUp("blue_button");
+                    break;
+                    case "yellow":
+                    lightUp("yellow_button");
+                    break; 
+                    case "green": 
+                    lightUp("green_button");
+                    break; 
+                }
+            }, time);
+            time += 1000;  
+        })
+    }
+
+    function lightUp(buttonName) {
+        $("." + buttonName).removeClass("opacity");
+        $("." + buttonName).addClass("notOpaque");
+        //lightOff(buttonName)
+        //setTimeout(lightOff, 5000)
+        setTimeout(function() { 
+            lightOff(buttonName); 
+        }, 1000);
+    }
+
+   
+
+    function lightOff(buttonName) {
+        $("." + buttonName).removeClass("notOpaque");
+        $("." + buttonName).addClass("opacity"); 
+    }    
+    //adds one to the computer array each time 
+
+    //takes what the user does into an array 
+    var userArray = []
+
+    function addEventsToButtons() {
+        $(".red_button").click(function() {
+            var colorChoice = $(this).attr("id");
+            lightUp("red_button");
+            createUserArray(colorChoice); 
+        });
+        $(".yellow_button").click(function() {
+            var colorChoice = $(this).attr("id");
+            lightUp("yellow_button");
+            createUserArray(colorChoice); 
+        });
+        $(".blue_button").click(function(){
+            var colorChoice = $(this).attr("id");
+            lightUp("blue_button");
+            createUserArray(colorChoice); 
+        });
+        $(".green_button").click(function() {
+            var colorChoice = $(this).attr("id");
+            createUserArray(colorChoice); 
+            lightUp("green_button");
+        })
+        
+    }
+
+    function createUserArray(choice) {
+        switch(choice) {
         case "red": 
-        lightUp("red_button")
+        userArray.push("red");
+        break; 
+        case "yellow": 
+        userArray.push("yellow")
+        break; 
         case "blue": 
-        lightUp("blue_button")
-        case "yellow":
-        lightUp("yellow_button")
-        case "green": 
-        lightUp("green_button")
-    } 
-}
-//adds one to the computer array each time 
-
-//takes what the user does into an array 
-var user_array = []
-
-function user_choice() {
-    $("#.red_button").onClick(function {
-        var choice = "red" 
-    })
-}
-
-function user_array_choice() {
-    switch(choice)
-    case ()
-}
+        userArray.push("blue")
+        break; 
+        case "green":
+        userArray.push("green");
+        break; 
+        }
+        userComputerCompare() 
+    }
 //let's the user submit their choices 
 
 //compares what the computer did and what the user did 
+    function userComputerCompare() {
+        var i 
+        var inputCorrect = true 
+        for (i = 0; i < userArray.length; i++) {
+            if (userArray[i] != computerArray[i]) {
+                inputCorrect = false 
+                alert("You got it wrong")
+                break 
+            }
+        }
+    //this part of the function checks if the user has not only entered a correct answer
+    //but entered as many correct answers as are in the computer array
+        if (inputCorrect) {
+            if (userArray.length == computerArray.length) {
+                userArray = [] 
+                addToComputerArray() 
+            }
+        }
+    }
 
 //takes the comparison and displays if you got it or not 
 
@@ -75,6 +152,4 @@ function user_array_choice() {
 //     setTimeout( function() {
 //       lightUp(color)
 //     }, 1000)
-// }
-
-lightUp(".red_button")
+})
